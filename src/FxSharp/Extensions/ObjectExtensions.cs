60098e0bb4851f234388bd96d831257fbe8928ab
Samuel Schuhmacher <forgetaboutit@muhbaasu.de>
2014-05-21 00:18:24 +0200

    Add basic Prelude and ObjectExtensions.

---
+++ src/FxSharp/Extensions/ObjectExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace FxSharp.Extensions
+{
+    public static class ObjectExtensions
+    {
+        public static T Identity<T>(this T t)
+        {
+            return Prelude.Id(t);
+        }
+
+        public static T Constant<T>(this T a, T b)
+        {
+            return Prelude.Const(a, b);
+        }
+    }
+}
--- src/FxSharp/Prelude.cs
+++ src/FxSharp/Prelude.cs
@@ -1,4 +1,28 @@
 ﻿namespace FxSharp
 {
-    public static class Prelude {}
+    public static class Prelude
+    {
+        /// <summary>
+        ///     The identity function.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        public static T Id<T>(T t)
+        {
+            return t;
+        }
+
+        /// <summary>
+        ///     A binary function that always returns its first argument.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static T Const<T>(T a, T b)
+        {
+            return a;
+        }
+    }
 }
