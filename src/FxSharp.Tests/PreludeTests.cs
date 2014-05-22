8d277c7572c6e5d330cacd3e45ce06471989d27d
Samuel Schuhmacher <sschuhmacher@synapticon.com>
2014-05-22 09:58:44 +0200

    Add tests for Prelude.

---
+++ src/FxSharp.Tests/PreludeTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FxSharp.Tests
+{
+    [TestClass]
+    public class PreludeTests
+    {
+        [TestMethod]
+        public void IdReturnThePlainArgument()
+        {
+            const string str = "";
+            const int five = 5;
+            var now = DateTime.Now;
+
+            Assert.AreSame(str, Prelude.Id(str));
+            Assert.AreEqual(five, Prelude.Id(five));
+            Assert.AreEqual(now, Prelude.Id(now));
+        }
+
+        [TestMethod]
+        public void ConstShouldAlwaysReturnTheFirstArgument()
+        {
+            const string str1 = "";
+            const string str2 = "abc";
+            const int five = 5;
+            const int ten = 10;
+            var now = DateTime.Now;
+            var today = DateTime.Today;
+
+            Assert.AreSame(str1, Prelude.Const(str1, str2));
+            Assert.AreEqual(five, Prelude.Const(five, ten));
+            Assert.AreEqual(now, Prelude.Const(now, today));
+        }
+    }
+}
