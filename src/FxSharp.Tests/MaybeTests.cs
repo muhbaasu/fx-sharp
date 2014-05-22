bea958a9a2bc4ef1e83cf4f54c38e84a68645aab
Samuel Schuhmacher <sschuhmacher@synapticon.com>
2014-05-22 13:19:14 +0200

    Add missing tests for Maybe.

--- src/FxSharp.Tests/MaybeTests.cs
+++ src/FxSharp.Tests/MaybeTests.cs
@@ -1,10 +1,71 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace FxSharp.Tests
 {
     [TestClass]
     public class MaybeTests
     {
+        internal enum TestEnum
+        {
+            DefaultValue,
+            AnotherValue
+        }
+
+        [TestMethod]
+        public void ToMaybeShouldReturnNothingForNull()
+        {
+            object o = null;
+
+            // ReSharper disable once ExpressionIsAlwaysNull
+            var result = o.ToMaybe().Match(just: _ => true, nothing: () => false);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void ToMaybeShouldReturnJustForNotNull()
+        {
+            var o = new object();
+            var result = o.ToMaybe().Match(just: _ => true, nothing: () => false);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void ToMaybeShouldReturnJustForValueTypes()
+        {
+            const int five = 5;
+            var result = five.ToMaybe().Match(just: _ => true, nothing: () => false);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void ToMaybeShouldReturnJustForEnumValues()
+        {
+            const TestEnum defaultValue = TestEnum.DefaultValue;
+            const TestEnum anotherValue = TestEnum.AnotherValue;
+
+            Assert.IsTrue(defaultValue.ToMaybe().Match(just: _ => true, nothing: () => false));
+            Assert.IsTrue(anotherValue.ToMaybe().Match(just: _ => true, nothing: () => false));
+        }
+
+        [TestMethod]
+        public void ToEnumerableShouldHaveOneElementForJust()
+        {
+            var justFive = 5.ToMaybe();
+            Assert.AreEqual(1, justFive.ToEnumerable().Count());
+        }
+
+        [TestMethod]
+        public void ToEnumerableShouldHaveZeroElementsForNothing()
+        {
+            var nothing = Maybe.Nothing<object>();
+            Assert.AreEqual(0, nothing.ToEnumerable().Count());
+        }
+
         [TestMethod]
         public void Select_DoNotMapFnOnlyWhenNothing()
         {
@@ -123,6 +184,74 @@ namespace FxSharp.Tests
                 just: _ => false,
                 nothing: () => true);
             Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void SelectManyShouldPropatateAValueFromNestedMaybes()
+        {
+            var justFive = 5.ToMaybe();
+            var squareFn = new Func<int, Maybe<int>>(i => Maybe.Just(i*i));
+            var result = justFive.SelectMany(squareFn).GetOrElse(-1);
+
+            Assert.AreEqual(25, result);
+        }
+
+        [TestMethod]
+        public void SelectManyShouldPropagateFailureFromNestedMaybes()
+        {
+            var justFive = 5.ToMaybe();
+            var failFn = new Func<int, Maybe<int>>(_ => Maybe.Nothing<int>());
+            var result = justFive.SelectMany(failFn).GetOrElse(-1);
+
+            Assert.AreEqual(-1, result);
+        }
+
+        [TestMethod]
+        public void SelectManyShouldJustReturnFailure()
+        {
+            var nothing = Maybe.Nothing<object>();
+            var called = false;
+            var maybeIdentity = new Func<object, Maybe<object>>(o =>
+            {
+                called = true;
+                return o.ToMaybe();
+            });
+
+            nothing.SelectMany(maybeIdentity);
+
+            Assert.IsFalse(called);
+        }
+
+        [TestMethod]
+        public void SelectManyShouldWorkWithSyntax()
+        {
+            var maybeFive = 5.ToMaybe();
+            var maybeFourtyTwo = 42.ToMaybe();
+            var result = from five in maybeFive
+                         from fourtyTwo in maybeFourtyTwo
+                         select five + fourtyTwo;
+
+            Assert.AreEqual(47, result.GetOrElse(-1));
+        }
+
+        [TestMethod]
+        public void GetOrElseTest()
+        {
+            var justFive = 5.ToMaybe();
+            var nothing = Maybe.Nothing<int>();
+
+            Assert.AreEqual(5, justFive.GetOrElse(-1));
+            Assert.AreEqual(-1, nothing.GetOrElse(-1));
+        }
+
+        [TestMethod]
+        public void ToStringTest()
+        {
+            var justFive = 5.ToMaybe();
+            var nothing = Maybe.Nothing<int>();
+
+            Assert.AreEqual("Just 5", justFive.ToString());
+            Assert.AreEqual("Nothing", nothing.ToString());
         }
     }
 }
