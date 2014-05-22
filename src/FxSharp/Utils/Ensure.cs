1631bcf0bfc6fa323c1d66cedef7603b5eabe259
Samuel Schuhmacher <sschuhmacher@synapticon.com>
2014-05-22 14:34:32 +0200

    Add null-check helper and Jetbrains annotations.

---
+++ src/FxSharp/Utils/Ensure.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FxSharp.Utils
+{
+    internal class Ensure
+    {
+        public static void NotNull<T>(T o, string source)
+        {
+            // ReSharper disable once CompareNonConstrainedGenericWithNull
+            if (typeof (T).IsClass && o == null)
+            {
+                throw new ArgumentNullException(source);
+            }
+        }
+    }
+}
