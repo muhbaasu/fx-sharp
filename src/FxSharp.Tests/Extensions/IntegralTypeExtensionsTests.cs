aae63d8626d12c8f90c0fda9141eaea53dc61be5
Samuel Schuhmacher <sschuhmacher@synapticon.com>
2014-05-22 09:58:14 +0200

    Reformat tests for IntegralTypeExtension.

--- src/FxSharp.Tests/Extensions/IntegralTypeExtensionsTests.cs
+++ src/FxSharp.Tests/Extensions/IntegralTypeExtensionsTests.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace FxSharp.Tests.Extensions
 {
     /// <summary>
-    /// Internal test enum.
+    ///     Internal test enum.
     /// </summary>
     internal enum DaysInt
     {
@@ -16,29 +16,29 @@ namespace FxSharp.Tests.Extensions
     [TestClass]
     public class IntegralTypeExtensionsTests
     {
-
         /// <summary>
-        /// Test if the parsing of an integer value returns Just when the 
-        /// given value is not in range of the given enum type.
+        ///     Test if the parsing of an integer value returns Just when the
+        ///     given value is not in range of the given enum type.
         /// </summary>
         [TestMethod]
         public void ParseEnumInt()
         {
-            var value = (int) DaysInt.Mon;
+            const int value = (int) DaysInt.Mon;
+
             value.ParseEnum<DaysInt>().Match_(
                 just: day => Assert.AreEqual(day, DaysInt.Mon),
                 nothing: () => Assert.Fail("Option should be nothing!"));
-
         }
 
         /// <summary>
-        /// Test if the parsing of an integer value returns Nothing when the 
-        /// given value is not in range of the given enum type.
+        ///     Test if the parsing of an integer value returns Nothing when the
+        ///     given value is not in range of the given enum type.
         /// </summary>
         [TestMethod]
         public void ParseEnumIntFailsWhenNotInRange()
         {
-            var day = 3;
+            const int day = 3;
+
             day.ParseEnum<DaysInt>()
                 .Select_(_ => Assert.Fail("Option should be nothing!"));
         }
