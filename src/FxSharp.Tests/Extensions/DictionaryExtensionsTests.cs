d174f14a83fc3349f2bec43f77197f0fff982e09
marcel_buesing <mbuesing@synapticon.com>
2014-05-22 13:17:10 +0200

    Add unit tests for dictionary extensions.

---
+++ src/FxSharp.Tests/Extensions/DictionaryExtensionsTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using FxSharp.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FxSharp.Tests.Extensions
+{
+    [TestClass]
+    public class DictionaryExtensionsTests
+    {
+        private readonly Dictionary<int, int> _dict = new Dictionary<int, int>
+        {
+            {1, 10},
+            {2, 20}
+        };
+
+        [TestMethod]
+        public void GetMaybeShouldReturnJustWhenInRange()
+        {
+            _dict.GetMaybe(2)
+                .Otherwise_(() => Assert.Fail("Function should not have been invoked."));
+        }
+
+        [TestMethod]
+        public void GetMaybeShouldReturnNothingWhenNotInRange()
+        {
+            _dict.GetMaybe(3)
+                .Select_(_ => Assert.Fail("Function should not have been invoked."));
+        }
+    }
+}
