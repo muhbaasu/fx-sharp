3442daa413749d81004f2e22099a633c67317c73
Samuel Schuhmacher <sschuhmacher@synapticon.com>
2014-05-22 14:00:52 +0200

    Add FxAssert and basic Either test.

---
+++ src/FxSharp.Tests/EitherTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FxSharp.Tests
+{
+    [TestClass]
+    public class EitherTests
+    {
+        [TestMethod]
+        public void ToStringTest()
+        {
+            var left = Either.Left<int, object>(5);
+            var right = Either.Right<object, int>(10);
+            var invalid = new Either<object, object>();
+
+            Assert.AreEqual("Left 5", left.ToString());
+            Assert.AreEqual("Right 10", right.ToString());
+            FxAssert.Throws<InvalidOperationException, string>(invalid.ToString);
+        }
+    }
+}
---
+++ src/FxSharp.Tests/FxAssert.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FxSharp.Tests
+{
+    public static class FxAssert
+    {
+        public static void Throws<T, TRes>(Func<TRes> fn) where T : Exception
+        {
+            try
+            {
+                fn();
+            }
+            catch (T)
+            {
+                Assert.Fail();
+            }
+        }
+
+        public static void Throws<T>(Action fn) where T : Exception
+        {
+            try
+            {
+                fn();
+            }
+            catch (T)
+            {
+                Assert.Fail();
+            }
+        }
+    }
+}
