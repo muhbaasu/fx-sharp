89310132b51cde5d65144ea5b6c9cb7c358185e1
mbuesing <buesing.marcel@googlemail.com>
2014-06-03 20:06:12 +0200

    Add PopOrNothing to StackExtensions.

---
+++ src/FxSharp.Tests/Extensions/StackExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using FxSharp.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FxSharp.Tests.Extensions
+{
+    [TestClass]
+    public class StackExtensions
+    {
+        private readonly Stack<int> _empty = new Stack<int>();
+        private readonly Stack<int> _stack = new Stack<int>();
+
+        public StackExtensions()
+        {
+            _stack.Push(1);
+            _stack.Push(2);
+            _stack.Push(3);
+        }
+
+
+        [TestMethod]
+        public void PopShouldReturnNothingWhenStackIsEmpty()
+        {
+            Assert.IsTrue(_empty.PopOrNothing().IsNothing());
+        }
+
+        [TestMethod]
+        public void PopShouldReturnJustWhenStackNotEmpty()
+        {
+            Assert.IsTrue(_stack.PopOrNothing().IsJust());
+        }
+
+        [TestMethod]
+        public void PopShouldReturnCorrectValue()
+        {
+            _stack.PopOrNothing().Match_(
+                just: top => Assert.AreEqual(3, top),
+                nothing: () => Assert.Fail("Should not be nothing"));
+        }
+    }
+}
---
+++ src/FxSharp/Extensions/StackExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FxSharp.Utils;
+using JetBrains.Annotations;
+
+namespace FxSharp.Extensions
+{
+    public static class StackExtensions
+    {
+        /// <summary>
+        ///     Remove and return the object at the top of the stack.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the stack.</typeparam>
+        /// <param name="source">The stack.</param>
+        /// <returns>Maybe.Nothing(T) if stack is empty.</returns>
+        public static Maybe<TSource> PopOrNothing<TSource>([NotNull] this Stack<TSource> source)
+        {
+            Ensure.NotNull(source, "source");
+
+            return source.Any() ? Maybe.Just(source.Pop()) : Maybe.Nothing<TSource>();
+        }
+    }
+}
